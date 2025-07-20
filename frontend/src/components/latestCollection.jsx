c8abdb82ef6c4c3199a09cc7254c781b61405908
Gadiel de Sá <gadieldesa@gmail.com>
2025-07-20 18:31:25 -0300

    Best Seller Section Done

--- frontend/src/components/latestCollection.jsx
+++ frontend/src/components/latestCollection.jsx
@@ -15,7 +15,7 @@ const LatestCollection = () => {
   return (
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
-        <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+        <Title text1={'ÚLTIMOS'} text2={'LANÇAMENTOS'}/>
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a auctor diam, non rhoncus neque. 
         </p>
